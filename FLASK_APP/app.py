3deb322c6ca52dd9599a24c07e388344dbc66852
hoops92 <hoops92@users.noreply.github.com>
2020-03-05 15:35:43 -0700

    .pkl files and app

--- FLASK_APP/app.py
+++ FLASK_APP/app.py
@@ -1,20 +1,26 @@
-from flask import Flask, request, jsonify
 import pandas as pd
-from FLASK_APP import utils
+from flask import Flask, jsonify, request
 import pickle
 
-pickle_dict = pickle.load(open("dict.p", "rb"))
+# load machine leearning model
+model = pickle.load(open('tfidf.pkl','rb'))
 
+# app
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-    @app.route('/strain/<strain>', methods=['GET'])
-    def myfunc(strain):
-      #strain = request.values['strain']
-      return jsonify({"race":pickle_dict[strain]})
+# routes
+@app.route('/', methods=['POST'])
 
+def predict():
+    # get data
+    data = request.get_json(force=True)
 
-    return app
+    # convert data into dataframe
+    data.update((x, [y]) for x, y in data.items())
+    data_df = pd.DataFrame.from_dict(data)
 
-if __name__ == "__main__":
-    app = create_app()
+    # predictions
+    result = model.predict(data_df)
+
+    # send back to browser
+    output = {'results': int(result[0])}
